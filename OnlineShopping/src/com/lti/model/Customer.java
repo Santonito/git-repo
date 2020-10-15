8944c67eff7b3c735f933ad43949d0ffbd40aa6a
sayan <sayan@192.168.1.103>
2020-10-15 15:45:25 +0530

    added

---
+++ OnlineShopping/src/com/lti/model/Customer.java
@@ -0,0 +1,21 @@
+package com.lti.model;
+
+public class Customer {
+	
+	int cusId;
+	String cusName;
+	public int getCusId() {
+		return cusId;
+	}
+	public void setCusId(int cusId) {
+		this.cusId = cusId;
+	}
+	public String getCusName() {
+		return cusName;
+	}
+	public void setCusName(String cusName) {
+		this.cusName = cusName;
+	}
+
+}
+
